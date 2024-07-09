0984f3f66fe9bb71509d788e1023980d3574b39f
김세현 <allezvvell@gmail.com>
2024-07-09 21:17:31 +0900

    add redirects file

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const API_KEY = 'c9a1f2b800e74037a62f70fd4762c1de';
 let newsList = [];
 
 async function fetchNews() {
-  const url = new URL(
-    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
-  );
+  const url = new URL(`https://noona-js-news.netlify.app/top-headlines`);
   try {
     const response = await fetch(url);
     const data = await response.json();
     newsList = data.articles;
+    console.log(newsList);
   } catch (error) {
     console.log(error);
   }
